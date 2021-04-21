de00987629b0ff7c561b02deea676106f502d65a
Monica Salamanca <moni.salamanca@gmail.com>
2021-04-20 20:46:48 -0400

    Add Todo finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { AddTodoForm } from './AddTodoForm';
+import { TodoList } from './TodoList';
+import { AddTodo, Todo, ToggleTodo } from './types';
+
+const initialTodos: Array<Todo> = [
+  { text: "Walk the dog", complete: true }, 
+  { text: "Feed the cat", complete: false }
+];
 
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo: ToggleTodo = selectedTodo => {
+    const newTodos = todos.map(todo => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete
+        }
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  }
+
+  const addTodo: AddTodo = newTodo => {
+    newTodo.trim() !== "" && 
+    setTodos([ ...todos, { text: newTodo, complete: false } ])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <AddTodoForm addTodo={addTodo} />
+    </>
   );
 }
 
